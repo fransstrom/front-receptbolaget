8dca2debad69dd14285cf50f83364c9b6ffd7ae9
Unknown <frans.herrstrom@gmail.com>
2018-10-04 21:24:55 +0200

    Deleted unnecessary eventhandlers in recipe form
    eventhandles no longer take 200ms

--- src/Components/Admin/admin.js
+++ src/Components/Admin/admin.js
@@ -10,16 +10,19 @@ export default class Admin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        ingredients: [],
-        newRecipe: {
+      ingredients: [],
+    };
+
+    this.newRecipe={
+     
         Name: '',
         Description: '',
         Ingredients: ingredList,
         Instruktioner: '',
         IMGUrl: '',
         category: []
-      }
-    };
+      
+    }
 
     this.checkBoxes = {
       vegansk: false,
@@ -30,22 +33,7 @@ export default class Admin extends Component {
 
     this.term = '';
     this.ingredientSearch(this.term);
-
-    this.onNameChange = this.onNameChange.bind(this);
-    this.onDescriptionChange = this.onDescriptionChange.bind(this);
-    this.onInstructionChange = this.onInstructionChange.bind(this);
-    this.onIMGUrlChange = this.onIMGUrlChange.bind(this);
   }
-
-  //Använda för att skapa ingredlist i newrecipe istället för ingredlistitem functionen?
-  // componentDidMount(){
-  //   $(document).ready(function(){
-  //     $("body").on("click",'.add_btn',function(event){
-  //       console.log("helloo");
-  //     }.bind(this));
-
-  //   });
-  // }
 
   ingredientSearch(term) {
     fetch(`http://localhost:3000/allaingreds/${term}`)
@@ -56,7 +44,7 @@ export default class Admin extends Component {
   }
 
   postRecipe() {
-    let recipe = JSON.stringify(this.state.newRecipe);
+    let recipe = JSON.stringify(this.newRecipe);
     fetch('http://localhost:3000/saverecipe/', {
       method: 'POST',
       headers: {
@@ -76,28 +64,7 @@ export default class Admin extends Component {
       .catch(e => console.log(e, 'error'));
   }
 
-  onNameChange(event) {
-    let newRecipe = { ...this.state.newRecipe };
-    newRecipe.Name = event.target.value;
-    this.setState({ newRecipe });
-  }
 
-  onDescriptionChange(event) {
-    let newRecipe = { ...this.state.newRecipe };
-    newRecipe.Description = event.target.value;
-    this.setState({ newRecipe });
-  }
-
-  onInstructionChange(event) {
-    let newRecipe = { ...this.state.newRecipe };
-    newRecipe.Instruktioner = event.target.value;
-    this.setState({ newRecipe });
-  }
-  onIMGUrlChange(event) {
-    let newRecipe = { ...this.state.newRecipe };
-    newRecipe.IMGUrl = event.target.value;
-    this.setState({ newRecipe });
-  }
 
   render() {
     const ingredientSearch = _.debounce(term => {
@@ -105,150 +72,148 @@ export default class Admin extends Component {
     }, 300);
 
     return (
-     
-        <Row>
-          <h4 className="header">Du som admin kan lägga till recept</h4>
-          <p className="warning">Du lägger alltid till mängden ingredienser för 1 person</p>
-          <Col s={6} m={6} l={6}>
-            <form
+      <Row>
+        <h4 className="header">Du som admin kan lägga till recept</h4>
+        <p className="warning">
+          Du lägger alltid till mängden ingredienser för 1 person
+        </p>
+        <Col s={6} m={6} l={6}>
+          <form
             autocomplete="off"
-              ref="form"
-              onSubmit={e => {
-                e.preventDefault();
-                this.postRecipe(this.newRecipe);
-                console.log(this.newRecipe, 'KLICK');
-              }}>
+            ref="form"
+            onSubmit={e => {
+              e.preventDefault();
+              this.postRecipe(this.newRecipe);
+              console.log(this.newRecipe, 'KLICK');
+            }}>
+            <Input
+              required
+        
+              id="name"
+              name="Name"
+      
+              cat="newRecipe"
+              onChange={e=>this.newRecipe.Name=e.target.value}
+              s={12}
+              m={12}
+              l={12}
+              label="Namn för receptet">
+              <Icon>import_contacts</Icon>
+            </Input>
+            <Input
+              required  
+              name="Description"
+              onChange={e=>this.newRecipe.Description=e.target.value}
+              s={12}
+              m={12}
+              l={12}
+              type="textarea"
+              label="Beskrivning">
+              <Icon>description</Icon>
+            </Input>
+            <SearchBar
+              recipes={this.state.ingredients}
+              placeholder="Sök ingrediens"
+              onSearchTermChange={ingredientSearch}
+            />
+            <Col id="added" s={12} m={12} l={12} />
+            <Input
+              required
+              name="Instruktioner"
+              onChange={e=>this.newRecipe.Instruktioner=e.target.value}
+              type="textarea"
+              label="Instruktioner"
+              s={12}
+              m={12}
+              l={12}>
+              <Icon>more_vert</Icon>
+            </Input>
+            <Input
+              required
+             
+              onChange={e=>this.newRecipe.IMGUrl=e.target.value}
+              type="text"
+              label="Bild-länk"
+              icon="search"
+              s={12}
+              m={12}
+              l={12}>
+              <Icon>insert_link</Icon>
+            </Input>
+            <Row>
               <Input
-                required
-                value={this.state.newRecipe.Name}
-                id="name"
-                name="Name"
-                cat="newRecipe"
-                onChange={this.onNameChange}
-                s={12}
-                m={12}
-                l={12}
-                label="Namn för receptet">
-                <Icon>import_contacts</Icon>
-              </Input>
-              <Input
-                required
-                value={this.state.newRecipe.Description}
-                name="description"
-                onChange={this.onDescriptionChange}
-                s={12}
-                m={12}
-                l={12}
-                type="textarea"
-                label="Beskrivning">
-                <Icon>description</Icon>
-              </Input>
-              <SearchBar
-                recipes={this.state.ingredients}
-                placeholder="Sök ingrediens"
-                onSearchTermChange={ingredientSearch}
+                name="vegansk"
+                type="checkbox"
+                value="vegansk"
+                label="Vegansk"
+                checked={this.checkBoxes.vegansk}
+                onChange={e => {
+                  if (e.target.checked) {
+                    this.state.newRecipe.category.push(e.target.name);
+                  } else {
+                    this.state.newRecipe.category.pop(e.target.name);
+                  }
+                  console.log(this.state.newRecipe);
+                }}
               />
-               <Col id="added" s={12} m={12} l={12} />
               <Input
-                required
-                value={this.state.newRecipe.Instruktioner}
-                name="Instruktioner"
-                onChange={this.onInstructionChange}
-                type="textarea"
-                label="Instruktioner"
-                s={12}
-                m={12}
-                l={12}>
-                <Icon>more_vert</Icon>
-              </Input>
+                name="vegetarisk"
+                type="checkbox"
+                value="vegetarisk"
+                label="Vegetarisk"
+                checked={this.checkBoxes.vegitarisk}
+                onChange={e => {
+                  if (e.target.checked) {
+                    this.newRecipe.category.push(e.target.name);
+                  } else {
+                    this.newRecipe.category.pop(e.target.name);
+                  }
+                  console.log(this.newRecipe);
+                }}
+              />
               <Input
-                required
-                value={this.state.newRecipe.IMGUrl}
-                onChange={this.onIMGUrlChange}
-                type="text"
-                label="Bild-länk"
-                icon="search"
-                s={12}
-                m={12}
-                l={12}>
-                <Icon>insert_link</Icon>
-              </Input>
-              <Row>
-                <Input
-                  name="vegansk"
-                  type="checkbox"
-                  value="vegansk"
-                  label="Vegansk"
-                  checked={this.checkBoxes.vegansk}
-                  onChange={e => {
-                    if (e.target.checked) {
-                      this.state.newRecipe.category.push(e.target.name);
-                    } else {
-                      this.state.newRecipe.category.pop(e.target.name);
-                    }
-                    console.log(this.state.newRecipe);
-                  }}
-                />
-                <Input
-                  name="vegetarisk"
-                  type="checkbox"
-                  value="vegetarisk"
-                  label="Vegetarisk"
-                  checked={this.checkBoxes.vegitarisk}
-                  onChange={e => {
-                    if (e.target.checked) {
-                      this.state.newRecipe.category.push(e.target.name);
-                    } else {
-                      this.state.newRecipe.category.pop(e.target.name);
-                    }
-                    console.log(this.state.newRecipe);
-                  }}
-                />
-                <Input
-                  name="glutenfri"
-                  type="checkbox"
-                  value="glutenfri"
-                  label="Glutenfri"
-                  checked={this.checkBoxes.glutenfri}
-                  onChange={e => {
-                    if (e.target.checked) {
-                      this.state.newRecipe.category.push(e.target.name);
-                    } else {
-                      this.state.newRecipe.category.pop(e.target.name);
-                    }
-                    console.log(this.state.newRecipe);
-                  }}
-                />
-                <Input
-                  name="laktosfri"
-                  type="checkbox"
-                  value="laktosfri"
-                  label="Laktosfri"
-                  checked={this.checkBoxes.laktosfri}
-                  onChange={e => {
-                    if (e.target.checked) {
-                      this.state.newRecipe.category.push(e.target.name);
-                    } else {
-                      this.state.newRecipe.category.pop(e.target.name);
-                    }
-                    console.log(this.state.newRecipe);
-                  }}
-                />
-              </Row>
-              <Button waves="red" className="blue" type="submit">
-                Skicka
-                <Icon right>send</Icon>
-              </Button>
-            </form>
-          </Col>
-          <Col s={6} m={6} l={6}>
-            <Collection className="ingredientsAdd">
-              <IngredientList ingredients={this.state.ingredients} />
-            </Collection>
-          </Col>
-         
-        </Row>
-     
+                name="glutenfri"
+                type="checkbox"
+                value="glutenfri"
+                label="Glutenfri"
+                checked={this.checkBoxes.glutenfri}
+                onChange={e => {
+                  if (e.target.checked) {
+                    this.newRecipe.category.push(e.target.name);
+                  } else {
+                    this.newRecipe.category.pop(e.target.name);
+                  }
+                  console.log(this.newRecipe);
+                }}
+              />
+              <Input
+                name="laktosfri"
+                type="checkbox"
+                value="laktosfri"
+                label="Laktosfri"
+                checked={this.checkBoxes.laktosfri}
+                onChange={e => {
+                  if (e.target.checked) {
+                    this.newRecipe.category.push(e.target.name);
+                  } else {
+                    this.newRecipe.category.pop(e.target.name);
+                  }
+                  console.log(this.newRecipe);
+                }}
+              />
+            </Row>
+            <Button waves="red" className="blue" type="submit">
+              Skicka
+              <Icon right>send</Icon>
+            </Button>
+          </form>
+        </Col>
+        <Col s={6} m={6} l={6}>
+          <Collection className="ingredientsAdd">
+            <IngredientList ingredients={this.state.ingredients} />
+          </Collection>
+        </Col>
+      </Row>
     );
   }
 }
